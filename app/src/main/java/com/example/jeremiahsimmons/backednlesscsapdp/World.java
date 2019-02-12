85e973857fdd8b698885b6ede30af359d904b4a1
Jeremiah Simmons <jsimmons@psdr3.org>
2019-02-12 13:58:28 -0600

    Updated World

--- app/src/main/java/com/example/jeremiahsimmons/backednlesscsapdp/World.java
+++ app/src/main/java/com/example/jeremiahsimmons/backednlesscsapdp/World.java
@@ -4,4 +4,19 @@ public class World {
   
   
   //This is a comment.  It's great.
+
+    private String description;
+
+    public World(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
